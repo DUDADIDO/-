93d7dd912310ed86ebaf39b2054b936700cd5250
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-10-24 01:35:53 +0900

    feat: 게시판 틀 완성

--- front/src/components/communitypage/CommunityArticle.jsx
+++ front/src/components/communitypage/CommunityArticle.jsx
@@ -8,7 +8,7 @@ const dummy = CommunityDummy;
 
 export default function CommunityArticle() {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 10;
 
   const totalPages = Math.ceil(dummy.length / itemsPerPage);
 
@@ -22,16 +22,67 @@ export default function CommunityArticle() {
   };
 
   return (
-    <div>
+    <div className="w-full">
+      <div className="flex justify-between items-center font-bold">
+        <div className="w-1/12 text-center">No</div>
+        <div className="w-8/12 text-center">제목</div>
+        <div className="w-1/12 text-center">글쓴이</div>
+        <div className="w-1/12 text-center">작성시간</div>
+        <div className="w-1/12 text-center">조회수</div>
+      </div>
+      <hr className="my-1 border-0 dark:bg-gray-200 w-full h-0.5" />
       {currentItems.map((data) => (
-        <CommunityItem key={data.id} data={data} />
+        <div key={data.id} className="my-5">
+          <CommunityItem data={data} />
+        </div>
       ))}
-      <div className="mt-10">
+
+      <div className="mt-16 flex justify-center items-center space-x-2">
+        {/* Prev 버튼 */}
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          className={`text-sm font-semibold py-1 px-3 mx-1 rounded-lg transition-all duration-300 shadow-lg
+      ${
+        currentPage === 1
+          ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+          : "bg-gray-200 text-gray-700 hover:bg-blue-400 hover:text-white"
+      }
+    `}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+
+        {/* 페이지 번호 버튼 */}
         {Array.from({ length: totalPages }, (_, index) => (
-          <button key={index} onClick={() => handlePageChange(index + 1)}>
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={`text-sm font-semibold py-1 px-3 mx-1 rounded-lg transition-all duration-300 shadow-lg
+        ${
+          currentPage === index + 1
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-gray-700 hover:bg-blue-400 hover:text-white"
+        }`}
+          >
             {index + 1}
           </button>
         ))}
+
+        {/* Next 버튼 */}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          className={`text-sm font-semibold py-1 px-3 mx-1 rounded-lg transition-all duration-300 shadow-lg
+      ${
+        currentPage === totalPages
+          ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+          : "bg-gray-200 text-gray-700 hover:bg-blue-400 hover:text-white"
+      }
+    `}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
--- front/src/pages/CommunityPage.jsx
+++ front/src/pages/CommunityPage.jsx
@@ -2,10 +2,27 @@ import { useState, useEffect } from "react";
 
 import CommunityArticle from "@/components/communitypage/CommunityArticle.jsx";
 
+import { HiOutlinePencilAlt } from "react-icons/hi";
+
 export default function CommunityPage() {
   return (
-    <div>
-      <p>자유게시판</p>
+    <div className="flex flex-col justify-start items-center w-10/12 h-full">
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col justify-center items-start w-full">
+          <p className=" text-3xl font-semibold mt-10 mb-5">커뮤니티</p>
+          <p className="text-lg font-bold">
+            여러분의 자유로운 의견을 나눠보세요.
+          </p>
+        </div>
+        <div className="flex justify-end items-end w-full">
+          <div className="flex items-center justify-end text-xl font-semibold bg-appBlue2 px-3 py-1 rounded-lg hover:bg-blue-400 hover:text-white duration-200 hover:cursor-pointer">
+            <HiOutlinePencilAlt />
+            <p>&nbsp;&nbsp;</p>
+            <button>글쓰기</button>
+          </div>
+        </div>
+      </div>
+      <hr className="mt-4 mb-1 border-0 dark:bg-gray-500 w-full h-0.5" />
       <CommunityArticle />
     </div>
   );
